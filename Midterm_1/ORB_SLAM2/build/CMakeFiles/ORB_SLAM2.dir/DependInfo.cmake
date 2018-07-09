4cea0a4b795021f2bbfc14981b2e90ece7b7b77f
Hardi <hdesai2@ncsu.edu>
2018-07-09 15:16:26 -0400

    Adding code files

---
+++ Midterm_1/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/ORB_SLAM2/src/Converter.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Frame.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/FrameDrawer.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Initializer.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/KeyFrame.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/LocalMapping.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/LoopClosing.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Map.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/MapDrawer.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/MapPoint.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/ORBextractor.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/ORBmatcher.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Optimizer.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/PnPsolver.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Sim3Solver.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/System.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Tracking.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/nvidia/ORB_SLAM2/src/Viewer.cc" "/home/nvidia/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/home/nvidia/Pangolin/include"
+  "/home/nvidia/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1"
+  "/opt/ros/kinetic/include/opencv-3.3.1/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
